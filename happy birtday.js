3d48c93984bc2add1aa2d79c357b2a9ac5b3b871
Fidel Kyalo <145541010+Fidelkyalo@users.noreply.github.com>
2025-08-19 19:27:58 +0300

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY", "WAMBO", "MORE WINS AHEAD IN JESUS' NAME!"],
+    strings: ["HAPPY", "BIRTHDAY", "WAMBO", "BARAKA"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
@@ -399,4 +399,5 @@ window.addEventListener("resize", function () {
 
   ctx.font = opts.charSize + "px Verdana";
 });
+
 
